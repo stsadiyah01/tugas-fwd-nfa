71e863c537d59ce02116a4a717e23e72dc670fab
stsadiyah01 <sadiyah0531@gmail.com>
2025-09-28 00:01:46 +0700

    Tugas React 1

---
+++ Studi_kasus/booksales/src/pages/contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div className="container py-5">
+      <h1 className="fw-bold">Contact Us</h1>
+      <p>Hubungi Bookstore melalui sosial media atau isi form pertanyaan.</p>
+    </div>
+    
+  );
+}
